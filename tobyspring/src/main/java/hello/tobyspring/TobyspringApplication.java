1bf56d8e2d6d6069ea86a0bd89a8d003898702d2
Yoonhwan Kim <wesaq11@gmail.com>
2023-01-29 16:40:13 +0900

    refactoring String value

--- tobyspring/src/main/java/hello/tobyspring/TobyspringApplication.java
+++ tobyspring/src/main/java/hello/tobyspring/TobyspringApplication.java
@@ -3,6 +3,9 @@ package hello.tobyspring;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -28,8 +31,8 @@ public class TobyspringApplication {
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    resp.setStatus(200);
-                    resp.setHeader("Content-Type", "text/plain");
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
                     resp.getWriter().println("Hello Servlet");
                 }
             }).addMapping("/hello");
