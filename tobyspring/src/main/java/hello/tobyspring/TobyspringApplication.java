3e57d19bebe199591b28d96a9ec96abceba26cdf
Yoonhwan Kim <wesaq11@gmail.com>
2023-01-29 16:38:59 +0900

    Servlet 등록 후 테스트

--- tobyspring/src/main/java/hello/tobyspring/TobyspringApplication.java
+++ tobyspring/src/main/java/hello/tobyspring/TobyspringApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * 스프링 부트가 없이 설정해본다.
  */
@@ -18,7 +24,16 @@ public class TobyspringApplication {
          */
         ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
         // 서블릿컨테이너를 만드는 함수.
-        WebServer webServer = tomcatServletWebServerFactory.getWebServer();
+        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
         // 톰캣 서블릿 컨테이너 동작.
         webServer.start();
 
