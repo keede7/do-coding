87f21c972dfc65da2cd440b8de0a812a0fcfc7bf
Yoonhwan Kim <wesaq11@gmail.com>
2023-02-02 00:32:25 +0900

    DispatcherServlet으로 변경 후 매핑정보 사용하기

--- tobyspring/src/main/java/hello/tobyspring/HelloController.java
+++ tobyspring/src/main/java/hello/tobyspring/HelloController.java
@@ -1,7 +1,12 @@
 package hello.tobyspring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,6 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         // null이면 예외발생, 아니라면 그대로 사용하여 return
         return helloService.sayHello(Objects.requireNonNull(name));
--- tobyspring/src/main/java/hello/tobyspring/TobyspringApplication.java
+++ tobyspring/src/main/java/hello/tobyspring/TobyspringApplication.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -24,13 +26,13 @@ public class TobyspringApplication {
         System.out.println("Hello Containerless Standalone");
         // Application Context == Spring Container
         // 어셈블러를 가져오고
-        GenericApplicationContext genericApplicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         // 각 빈들을 등록시킨다.
-        genericApplicationContext.registerBean(HelloController.class);
+        applicationContext.registerBean(HelloController.class);
         // 등록할 때 구체적은 클래스 타입을 넣어줘야 하기 때문에 인터페이스가 아닌 구현체를 넣는다.
-        genericApplicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.registerBean(SimpleHelloService.class);
         // 처음에 가지고있는 구성정보를 초기화한다.
-        genericApplicationContext.refresh();
+        applicationContext.refresh();
 
         /**
          * 서블릿 컨테이너 생성하기.
@@ -40,33 +42,10 @@ public class TobyspringApplication {
         ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
         // 서블릿컨테이너를 만드는 함수.
         WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
-
-            // 모든 요청을 받고 위임처리 역할을 하는 front Controller 로 변경된다.
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능
-
-                    String requestURI = req.getRequestURI();
-                    String method = req.getMethod();
-
-                    if (requestURI.equals("/hello") && method.equals(HttpMethod.GET.name())) {
-                        // 요청을 받아 동적으로 url을 변경
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = genericApplicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        // 일반적으로 성공하면 200을 전달한다.
-//                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-                // 해당 프론트 컨트롤러는 모든 요청을 처리한다.
-            }).addMapping("/*");
+            // 디스패처 서블릿으로 변환한다.
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
         });
         // 톰캣 서블릿 컨테이너 동작.
         webServer.start();
