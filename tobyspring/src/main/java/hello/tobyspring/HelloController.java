64ff08e477a883c4a3edc5de9c09e5141c7e7936
Yoonhwan Kim <wesaq11@gmail.com>
2023-02-01 22:58:57 +0900

    DI Sample

--- tobyspring/src/main/java/hello/tobyspring/HelloController.java
+++ tobyspring/src/main/java/hello/tobyspring/HelloController.java
@@ -1,7 +1,11 @@
 package hello.tobyspring;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloHelloService = new SimpleHelloService();
+        // null이면 예외발생, 아니라면 그대로 사용하여 return
+        return helloHelloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ tobyspring/src/main/java/hello/tobyspring/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package hello.tobyspring;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
