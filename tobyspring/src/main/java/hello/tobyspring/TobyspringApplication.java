33c524bd31ac4028629be1f3a66b63c85260041f
Yoonhwan Kim <wesaq11@gmail.com>
2023-02-02 00:40:31 +0900

    스프링 컨테이너로 통합

--- tobyspring/src/main/java/hello/tobyspring/TobyspringApplication.java
+++ tobyspring/src/main/java/hello/tobyspring/TobyspringApplication.java
@@ -26,7 +26,24 @@ public class TobyspringApplication {
         System.out.println("Hello Containerless Standalone");
         // Application Context == Spring Container
         // 어셈블러를 가져오고
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+                // 서블릿컨테이너를 만드는 함수.
+                WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+                    // 디스패처 서블릿으로 변환한다.
+                    servletContext.addServlet("dispatcherServlet",
+                            // 자기자신인 ApplicationContext를 넣어준다.
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                // 톰캣 서블릿 컨테이너 동작.
+                webServer.start();
+            }
+        };
         // 각 빈들을 등록시킨다.
         applicationContext.registerBean(HelloController.class);
         // 등록할 때 구체적은 클래스 타입을 넣어줘야 하기 때문에 인터페이스가 아닌 구현체를 넣는다.
@@ -39,16 +56,16 @@ public class TobyspringApplication {
          * 스프링 부트가 톰캣서블릿 컨테이너를 내장해서 코드로 쉽게 시작해 사용하도록 만든 클래스가 있다.
          * TomcatServletWebServerFactory();
          */
-        ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-        // 서블릿컨테이너를 만드는 함수.
-        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
-            // 디스패처 서블릿으로 변환한다.
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-        });
-        // 톰캣 서블릿 컨테이너 동작.
-        webServer.start();
+//        ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+//        // 서블릿컨테이너를 만드는 함수.
+//        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+//            // 디스패처 서블릿으로 변환한다.
+//            servletContext.addServlet("dispatcherServlet",
+//                    new DispatcherServlet(applicationContext)
+//            ).addMapping("/*");
+//        });
+//        // 톰캣 서블릿 컨테이너 동작.
+//        webServer.start();
     }
 
 }
