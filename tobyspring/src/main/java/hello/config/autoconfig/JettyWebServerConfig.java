2c8aef9384914a3f2662eb976d5eb3194aeb4633
Yoonhwan Kim <wesaq11@gmail.com>
2023-02-11 16:15:54 +0900

    자동구성정보 대체하기

--- tobyspring/src/main/java/hello/config/autoconfig/JettyWebServerConfig.java
+++ tobyspring/src/main/java/hello/config/autoconfig/JettyWebServerConfig.java
@@ -2,21 +2,17 @@ package hello.config.autoconfig;
 
 import hello.config.ConditionalMyOnClass;
 import hello.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     // Tomcat 설정 클래스와 빈 이름이 동일해서 충돌이 나올 수 있다.
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
--- tobyspring/src/main/java/hello/config/autoconfig/TomcatWebServerConfig.java
+++ tobyspring/src/main/java/hello/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package hello.config.autoconfig;
 
 import hello.config.ConditionalMyOnClass;
 import hello.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +11,13 @@ import org.springframework.context.annotation.Bean;
 // 톰캣과 관련된 클래스가 존재하면 톰캣서버를 띄운다.
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+
+    // Conditional은 Method레벨에도 등록할 수 있다.
+    // 이미 등록된 동일한 타입이 존재하는지 보고
+    // 존재하지 않는다면 이 Bean을 등록해줘라 라고 하는것.
+    // 이 Conditional 판단 시점에는 유저구성정보(비즈니스 로직 빈들)이 이미 등록된 상태이다.
     @Bean
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
