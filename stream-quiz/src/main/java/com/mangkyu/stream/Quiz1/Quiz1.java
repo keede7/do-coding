488a12276a8b6fe3e97f30fc7c80e61aab5f18fc
Yoonhwan Kim <wesaq11@gmail.com>
2023-03-27 21:46:52 +0900

    quiz 1-2

--- stream-quiz/src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ stream-quiz/src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 public class Quiz1 {
 
     public static void main(String[] args) throws IOException {
-        new Quiz1().quiz1();
+        new Quiz1().quiz2();
     }
 
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
@@ -40,7 +40,16 @@ public class Quiz1 {
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> result = csvLines.stream()
+                .filter(user -> user[0].contains("정"))
+                .map(user -> user[1].trim())
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+                .collect(Collectors.toMap(Function.identity(), hobby -> 1, (before, after) -> before += after));
+
+        System.out.println("result = " + result);
+
+        return result;
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
