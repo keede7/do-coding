835b9de8121e6a13458c294896174be25eac8e94
Yoonhwan Kim <wesaq11@gmail.com>
2023-06-24 16:03:06 +0900

    Metric Example

--- spring-core-principal/actuator/src/main/java/hello/ActuatorApplication.java
+++ spring-core-principal/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,9 +1,12 @@
 package hello;
 
+import hello.order.v0.OrderConfigV0;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(OrderConfigV0.class)
+@SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
     public static void main(String[] args) {
