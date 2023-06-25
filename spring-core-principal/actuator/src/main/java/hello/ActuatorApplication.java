0dda09a35489b953bf19a52104d76ac4c1814234
Yoonhwan Kim <wesaq11@gmail.com>
2023-06-25 18:41:32 +0900

    Metrics #3

--- spring-core-principal/actuator/src/main/java/hello/ActuatorApplication.java
+++ spring-core-principal/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,11 +1,11 @@
 package hello;
 
-import hello.order.v0.OrderConfigV0;
+import hello.order.v3.OrderConfigV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV0.class)
+@Import(OrderConfigV3.class)
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
