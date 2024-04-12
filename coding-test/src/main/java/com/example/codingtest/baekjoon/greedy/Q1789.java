24521a34e21fb0fb81d29da522cf2371b0198a3b
yoonhwan <yoonhwan7@warmhilt.com>
2024-04-12 10:12:57 +0900

    백준 1789문제 해결

--- coding-test/src/main/java/com/example/codingtest/baekjoon/greedy/Q1789.java
+++ coding-test/src/main/java/com/example/codingtest/baekjoon/greedy/Q1789.java
@@ -3,16 +3,8 @@ package com.example.codingtest.baekjoon.greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-/**
- * 서로 다른 N개의 자연수의 합이 S라고 한다. S를 알 때, 자연수 N의 최댓값은 얼마일까?
- *
- * 첫째 줄에 자연수 S(1 ≤ S ≤ 4,294,967,295)가 주어진다.
- *
- * 첫째 줄에 자연수 N의 최댓값을 출력한다.
- */
 public class Q1789 {
 
     public static void main(String[] args) throws IOException {
@@ -21,34 +13,21 @@ public class Q1789 {
 
         long target = Long.parseLong(br.readLine());
 
-        Set<Long> collection = new HashSet<>();
+        long sum = 0;
+        long count = 0;
 
-        for (long num = 1; true; num++) {
+        for (long value = 1; true; value++) {
 
-            long tmp = target - num;
+            sum += value;
+            count++;
 
-            if (tmp < 0) {
+            if(sum > target) {
+                count--;
                 break;
             }
-
-            collection.add(num);
-            target -= num;
         }
 
-        boolean search = search(collection, target);
-
-        if(search) {
-            System.out.println(collection.size()+1);
-        }
+        System.out.println(count);
     }
 
-    private static boolean search(Set<Long> collection, long target) {
-
-        if(collection.contains(target)) {
-            collection.remove(target);
-            return search(collection, target + target);
-        }
-
-        return true;
-    }
 }
