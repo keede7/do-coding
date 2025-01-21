acbac09a91fd97a9676c3267d38f818657519ae1
keede7 <wesaq11@gmail.com>
2025-01-21 13:08:58 +0900

    Q16953

--- coding-test/src/main/java/com/example/codingtest/baekjoon/greedy/Q16953.java
+++ coding-test/src/main/java/com/example/codingtest/baekjoon/greedy/Q16953.java
@@ -1,5 +1,10 @@
 package com.example.codingtest.baekjoon.greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * 정수 A를 B로 바꾸려고 한다. 가능한 연산은 다음과 같은 두 가지이다.
  *
@@ -28,5 +33,36 @@ package com.example.codingtest.baekjoon.greedy;
  * 100 → 200 → 2001 → 4002 → 40021
  */
 public class Q16953 {
-    // 특징은..2로 나눴을 때 1이 남는
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int result = 1;
+
+        while (A != B) {
+            if(A > B) {
+                System.out.println("-1");
+                return;
+            }
+
+            String s = String.valueOf(B);
+
+            if(B % 2 == 0) {
+                B = B / 2;
+            } else if (s.charAt(s.length() - 1) == '1') {
+                s = s.substring(0, s.length() - 1);
+                B = Integer.parseInt(s);
+            } else {
+                System.out.println("-1");
+                return;
+            }
+
+            result++;
+        }
+
+        System.out.println(result);
+    }
 }
