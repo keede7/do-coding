79d7750ca1b2fcef6dca3a551ecc10c84ba4a215
keede7 <wesaq11@gmail.com>
2025-03-18 09:10:34 +0900

    feat: init Kotlin Starter

---
+++ kotlin-starter/src/test/kotlin/io/keede/kotlinstarter/KotlinStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.keede.kotlinstarter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinStarterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
