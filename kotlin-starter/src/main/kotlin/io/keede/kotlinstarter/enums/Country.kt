ec0e14f37a89c0f1535257f0ac15a5d1dcd4b723
keede7 <wesaq11@gmail.com>
2025-03-27 08:49:57 +0900

    feat: how to handle When Syntax By Enum Class

--- kotlin-starter/src/main/kotlin/io/keede/kotlinstarter/enums/Country.kt
+++ kotlin-starter/src/main/kotlin/io/keede/kotlinstarter/enums/Country.kt
@@ -6,5 +6,14 @@ enum class Country(
 
     KOREA("KO"),
     AMERICA("US")
+    ;
+
+    // Kotlin에서 when을 사용하면 좀 더 가독성 있게 사용이 가능하다.
+    private fun handleCountry(country: Country) {
+        when (country) {
+            KOREA -> println("")
+            AMERICA -> TODO()
+        }
+    }
 
 }
--- kotlin-starter/src/main/kotlin/io/keede/kotlinstarter/enums/JavaCountry.java
+++ kotlin-starter/src/main/kotlin/io/keede/kotlinstarter/enums/JavaCountry.java
@@ -24,4 +24,15 @@ public enum JavaCountry {
     public String getCode() {
         return code;
     }
+
+    // 코드가 많아지면 조건문 처리가 애매해진다
+    private static final void handleCountry(JavaCountry javaCountry) {
+        if(javaCountry == JavaCountry.KOREA) {
+
+        }
+
+        if (javaCountry == JavaCountry.AMERICA) {
+
+        }
+    }
 }
