4894a2d714b36543c9d008f6d0e6f5206801c0b7
keede7 <wesaq11@gmail.com>
2025-04-02 10:21:16 +0900

    feat: infix, inline, 지역함수

--- kotlin-starter/src/main/kotlin/io/keede/kotlinstarter/functions/FunctionUtil.kt
+++ kotlin-starter/src/main/kotlin/io/keede/kotlinstarter/functions/FunctionUtil.kt
@@ -5,6 +5,7 @@ package io.keede.kotlinstarter.functions
     1. 원본 클래스의 private, protected 멤버 접근 불가능
     2. 멤버함수, 확장함수 중 멤버함수에 우선권이 있다.
     3. 확장함수는 현재 타입을 기준으로 호출된다.
+    4. Java의 static을 쓰는 것처럼 사용이 가능하다.
  */
 // custom getter에도 사용이 가능하다. get() = ...
 fun String.lastChar(): Char {
@@ -16,4 +17,24 @@ fun main() {
     val s: String = "asdf"
 
     println(s.lastChar())
+
+    println()
+    // infix 함수
+    val value: Int = 3;
+    value add2 4
 }
+
+// infix 함수 = 중위함수, 새로운 함수를 호출하는 방법.
+// 예를들어 downTo, step이 있었다. '변수.함수이름(arg)' 대신 '변수 함수이름 arg'
+fun Int.add(other: Int): Int {
+    return this + other
+}
+
+infix fun Int.add2(other: Int): Int {
+    return this + other
+}
+
+/*
+    지역함수
+    함수 내에 함수를 정의한다. 하지만 depth가 깊어지므로 코드가 좋아보이진 않는다.
+ */
